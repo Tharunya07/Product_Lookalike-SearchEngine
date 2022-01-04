8ef6742bacee60ea84c36b614b126c1b8a649e68
Tharunya07 <74779411+Tharunya07@users.noreply.github.com>
2022-01-05 00:03:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 from jina import Document, DocumentArray, Flow
+
+def index():
+docs = DocumentArray.from_files('fashion_products') 
+#making a document array
+
+# Createing a Flow to process our Documents
